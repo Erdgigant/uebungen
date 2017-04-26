e944da6978909d15e45bb3287621ad438fa7239c
Roman Schilter <roman.schilter@internezzo.ch>
2017-04-26 09:54:23 +0200

    Added Webpack

---
+++ VueJS/4/hello/src/Mini.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="mini" draggable="true"
+		 :class="{hide: mini == $parent.miniStorage, dragEffect: dragEffect}"
+		 @dragenter="dragEffect = true" @dragleave="dragEffect = false" @dragstart="$parent.drag(mini, $event)">
+
+		<p class="header">
+			<span @click="$parent.remove(mini)">X</span>
+			<span @click="$parent.update(mini)">⟲</span>
+			<span @click="$parent.add(mini)">+</span>
+			<b v-if="mini.text">{{ mini.text }}</b>
+		</p>
+
+		<input type="text" @keyup.enter="$parent.update(mini)" v-model="mini.id"/>
+
+		<p v-if="mini.name">{{ mini.name }}</p>
+
+		<img v-if="mini.icon" :src="mini.icon"/>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['mini'],
+		data: function() {
+			return {
+				dragEffect: false
+			}
+		}
+	}
+</script>
